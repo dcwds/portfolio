58433c413be75d87d220fc3dd23a7c46290e47db
dcwoods <dcwds.co@gmail.com>
2019-04-30 21:48:48 -0400

    Add footer component.

---
+++ src/components/footer.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default () => (
+  <div className="footer">
+    <p>&copy; 2019</p>
+
+    <ul className="nav-items">
+      <li className="dribbble">
+        <a
+          href="https://dribbble.com/dcwoods"
+          rel="noopener noreferrer"
+          target="_blank">Dribbble</a>
+      </li>
+      <li className="github">
+        <a
+          href="https://github.com/daltonwoods"
+          rel="noopener noreferrer"
+          target="_blank">GitHub</a>
+      </li>
+      <li className="twitter">
+        <a
+          href="https://twitter.com/daltonwoods"
+          rel="noopener noreferrer"
+          target="_blank">Twitter</a>
+      </li>
+    </ul>
+  </div>
+)
