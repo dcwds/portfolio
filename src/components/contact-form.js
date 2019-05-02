87476c5d84658d6ed7abf9129a328853e21eca92
dcwoods <dcwds.co@gmail.com>
2019-05-02 12:39:58 -0400

    Basic contact form and styles. Uses Netlify as back-end.

---
+++ src/components/contact-form.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default () => (
+  <form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="bot-field" />
+    <input type="hidden" name="form-name" value="contact" />
+
+    <fieldset>
+      <label htmlFor="name">Full Name</label>
+      <input type="text" name="name" id="name" placeholder="Tyrion Lannister" />
+    </fieldset>
+
+    <fieldset>
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" placeholder="tyrion.lannister@gmail.com" />
+    </fieldset>
+
+    <fieldset>
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message" placeholder="Hello..." rows="6" />
+    </fieldset>
+
+    <input type="submit" value="Send Message" />
+  </form>
+)
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 import SEO from "../components/seo"
+import ContactForm from "../components/contact-form"
 
 export default () => (
   <Layout>
@@ -11,5 +12,7 @@ export default () => (
     />
 
     <h1>Contact</h1>
+
+    <ContactForm />
   </Layout>
 )
