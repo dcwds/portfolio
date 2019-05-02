bb860b1b7cdfed8299636495c7f58bd7bd74ef95
dcwoods <dcwds.co@gmail.com>
2019-05-01 23:00:06 -0400

    Refactor SEO component and add lang attribute to HTML tag.

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-const SEO = ({ title, description }) => (
+const SEO = ({ title, description, lang }) => (
   <StaticQuery
     query={ query }
     render={ ({
@@ -22,36 +22,59 @@ const SEO = ({ title, description }) => (
       }
 
       return (
-        <>
           <Helmet
+            htmlAttributes={{
+              lang
+            }}
             title={seo.title}
-            titleTemplate={ titleTemplate }>
-            <meta name="description" content={ seo.description } />
-
-            { seo.title && <meta property="og:title" content={ seo.title } /> }
-            { seo.description && (
-              <meta property="og:description" content={ seo.description } />
-            ) }
-            <meta name="twitter:card" content="summary_large_image" />
-            { twitterUsername && (
-              <meta name="twitter:creator" content={ twitterUsername } />
-            ) }
-          </Helmet>
-        </>
-      )
+            titleTemplate={ titleTemplate }
+            meta={[
+              {
+                name: "description",
+                content: seo.description
+              },
+              {
+                property: "og:title",
+                content: title
+              },
+              {
+                property: "og:description",
+                content: description
+              },
+              {
+                property: "og:type",
+                content: "website"
+              },
+              {
+                property: "twitter:card",
+                content: "summary"
+              },
+              {
+                property: "twitter:creator",
+                content: twitterUsername
+              },
+              {
+                property: "twitter:description",
+                content: description
+              }
+            ]}
+          />
+        )
+      }
     }
-  }
   />
 )
 
 export default SEO
 
 SEO.propTypes = {
+  lang: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string
 }
 
 SEO.defaultProps = {
+  lang: "en",
   title: null,
   description: null
 }
