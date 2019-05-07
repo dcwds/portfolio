1c68d7cd4d596919950ab36a4d3f6797e2819192
dcwoods <dcwds.co@gmail.com>
2019-05-07 13:46:21 -0400

    Rough footer visual design.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,34 +3,51 @@ import React from "react"
 export default () => (
   <>
     <footer className="navigation">
-      <p className="copyright">
-        &copy; 2019 Designed and coded from scratch using 
-        <a
-          href="https://gatsbyjs.org"
-          rel="noopener noreferrer"
-          target="_blank">Gatsby</a>
-      </p>
+      <section className="note">
+        <h3>Dalton Woods</h3>
 
-      <ul className="nav-items">
-        <li className="dribbble">
-          <a
-            href="https://dribbble.com/dcwoods"
-            rel="noopener noreferrer"
-            target="_blank">Dribbble</a>
-        </li>
-        <li className="github">
-          <a
-            href="https://github.com/daltonwoods"
-            rel="noopener noreferrer"
-            target="_blank">GitHub</a>
-        </li>
-        <li className="twitter">
-          <a
-            href="https://twitter.com/daltonwoods"
-            rel="noopener noreferrer"
-            target="_blank">Twitter</a>
-        </li>
-      </ul>
+        <p>
+          Product designer &amp; front-end developer.
+        </p>
+      </section>
+      
+      <section className="elsewhere-list">
+        <h3>Elsewhere.</h3>
+
+        <ul className="nav-items">
+          <li className="dribbble">
+            <a
+              href="https://dribbble.com/dcwoods"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Dribbble
+            </a>
+          </li>
+          <li className="github">
+            <a
+              href="https://github.com/daltonwoods"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </li>
+          <li className="twitter">
+            <a
+              href="https://twitter.com/daltonwoods"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <section className="copyright">
+        <small>&copy; Copyright 2019</small>
+      </section>
     </footer>
   </>
 )
