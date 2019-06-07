c4ea0ecd7dc2a9214d9e6ec0396f3664cc153c1e
dcwoods <dcwds.co@gmail.com>
2019-06-07 13:53:39 -0400

    Refactor Header component using TypeScript.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,35 @@
 import { Link } from "gatsby"
 import React, { Fragment } from "react"
 
-const Header = () => (
-  <Fragment>
-    <header className="navigation">
-      <ul className="nav-items">
-        <li>
-          <Link to="/work" activeClassName="active">
-            Work
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" activeClassName="active">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" activeClassName="active">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </header>
-  </Fragment>
-)
+const NavItems: Array<{
+  name: string
+  route: string
+  hasActiveClass: boolean
+}> = [
+  { name: "Work", route: "/work", hasActiveClass: true },
+  { name: "About", route: "/about", hasActiveClass: true },
+  { name: "Contact", route: "/contact", hasActiveClass: true }
+]
+
+const Header = () => {
+  const items = NavItems.map(item => (
+    <li key={item.name}>
+      <Link
+        to={item.route}
+        {...(item.hasActiveClass ? { activeClassName: "active" } : {})}
+      >
+        {item.name}
+      </Link>
+    </li>
+  ))
+
+  return (
+    <Fragment>
+      <header className="navigation">
+        <ul className="nav-items">{items}</ul>
+      </header>
+    </Fragment>
+  )
+}
 
 export default Header
