04bfd85de567f42fef7e3b4116e33254f89464d5
dcwoods <dcwds.co@gmail.com>
2019-06-10 23:07:53 -0400

    More work on theme selector button.

--- src/components/theme-selector.tsx
+++ src/components/theme-selector.tsx
@@ -1,14 +1,17 @@
 import React, { useContext } from "react"
 import ThemeContext from "../contexts/theme-context"
 
-import MoonIcon from "../assets/moon.inline.svg"
+import MoonIcon from "../assets/icon-theme-dark.svg"
+import SunIcon from "../assets/icon-theme-light.svg"
 
 const ThemeSelector = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
 
   return (
     <button className={`btn btn-theme btn-${theme}`} onClick={toggleTheme}>
-      <MoonIcon className="icon-dark" height="35" />
+      {theme === "theme-light" && <MoonIcon className="icon icon-dark" />}
+
+      {theme === "theme-dark" && <SunIcon className="icon icon-light" />}
     </button>
   )
 }
